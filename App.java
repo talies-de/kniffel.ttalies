91315d42207ff13c2f4e6425443bf066b6a40889
ttalies <thorsten@talies.de>
2020-10-18 20:11:40 +0200

    Dice block test

--- App.java
+++ App.java
@@ -1,81 +1,57 @@
 import java.util.*;
 import java.util.Arrays;
 
+class Tuple<K, V> {
+    public final K key;
+    public final V value;
+    public Tuple(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class Language {
-    static String ONES = "Einer";
-    static String TWOS = "Zweier";
-    static String THREES = "Dreier";
-    static String FOURS = "Vierer";
-    static String FIVES = "Fünfer";
-    static String SIXES = "Sechser";
+    static String ones = "Einer";
+    static String twos = "Zweier";
+    static String threes = "Dreier";
+    static String fours = "Vierer";
+    static String fives = "Fünfer";
+    static String sixes = "Sechser";
 
-    static String THREE_EQUALS = "Drei Gleiche";
-    static String FOUR_EQUALS = "Vier Gleiche";
-    static String FULL_HOUSE = "Volles Haus";
-    static String SMALL_STREET = "Kleine Straße";
-    static String BIG_STREET = "Große Straße";
-    static String KNIFFEL = "Kniffel";
-    static String CHANCE = "Chance";
+    static String threeEquals = "Drei Gleiche";
+    static String fourEquals = "Vier Gleiche";
+    static String fullHouse = "Volles Haus";
+    static String smallStreet = "Kleine Straße";
+    static String bigStreet = "Große Straße";
+    static String kniffel = "Kniffel";
+    static String chance = "Chance";
 
-    static String HORIZONTAL_RULER = "-----------------------";
-    static String DASHED_RULER     = "- - - - - - - - - - - -";
-    static String DOUBLE_RULER     = "=======================";
+    static String horizontalRuler = "-----------------------";
+    static String dashedRuler     = "- - - - - - - - - - - -";
+    static String doubleRuler     = "=======================";
 
-    static String PART_SUM_PART1 = "Teilsumme Teil 1: ";
-    static String SUM_PART1 = "Summe Teil 1: ";
-    static String SUM_PART2 = "Summe Teil 2: ";
-    static String BONUS = "Bonus Teil 1: ";
-    static String SUM = "Gesamtsumme : ";
+    static String partialSumPart1 = "Teilsumme Teil 1: ";
+    static String sumPart1 = "Summe Teil 1: ";
+    static String sumPart2 = "Summe Teil 2: ";
+    static String bonus = "Bonus Teil 1: ";
+    static String totalSum = "Gesamtsumme : ";
 
-    static String DICE = "Würfel";
+    static String dice = "Würfel";
 
     Language(){
         // only language definition
     }
 }    
 
-class Tuple<TypeKey, TypeValue> {
-    public final TypeKey key;
-    public final TypeValue value;
-    public Tuple(TypeKey key, TypeValue value) {
-        this.key = key;
-        this.value = value;
-    }
-}
-
 class KniffelSheet {
-    String[] part1 = {Language.ONES, Language.TWOS, Language.THREES, Language.FOURS, Language.FIVES, Language.SIXES};
-    String[] part2 = {Language.THREE_EQUALS, Language.FOUR_EQUALS, Language.FULL_HOUSE, Language.SMALL_STREET, Language.BIG_STREET, Language.KNIFFEL, Language.CHANCE};    
-}
-
-class Goal {
-    Tuple<String, Integer> set;
-}
-
-class Sheet {
-    ArrayList<Goal> game = new ArrayList<>();
-}
-
-class Player {
-    String name;
-
-    Sheet sheet;
-    Integer sumPart1 = 0;
-    Integer partSumPart1 = 0;
-    Integer sumPart2 = 0;
-    Integer bonusPart1 = 0;
-    Integer sum = 0;
-
-    DiceDeck deck = new DiceDeck();
-}
-
-class Players {
-    ArrayList<Player> player = new ArrayList<>();
+    String[] part1 = {Language.ones, Language.twos, Language.threes, Language.fours, Language.fives, Language.sixes};
+    String[] part2 = {Language.threeEquals, Language.fourEquals, Language.fullHouse, Language.smallStreet, Language.bigStreet, Language.kniffel, Language.chance};    
 }
 
 class Dice {
     Integer count;
     boolean roll;
+    Random r = new Random();
 
     public Dice() {
         this.count = 0;
@@ -83,8 +59,8 @@ class Dice {
     }
 
     public void roll() {
-        if (this.roll) {
-            this.count = (int)(Math.random() * 6) + 1;        
+        if (this.roll) {            
+            this.count = r.nextInt(6) + 1;            
         }
         else {
             // Roll not permitted
@@ -117,8 +93,32 @@ class DiceDeck {
     }
 }
 
-class Kniffel {
+class Goal {
+    Tuple<String, Integer> set;
+}
 
+class Sheet {
+    ArrayList<Goal> game = new ArrayList<>();
+}
+
+class Player {
+    String name;
+
+    Sheet sheet;
+    Integer sumPart1 = 0;
+    Integer partSumPart1 = 0;
+    Integer sumPart2 = 0;
+    Integer bonusPart1 = 0;
+    Integer sum = 0;
+
+    DiceDeck deck = new DiceDeck();
+}
+
+class Players {
+    ArrayList<Player> player = new ArrayList<>();
+}
+
+class Kniffel {
     private static int addPlayer(Players actualPlayers, String name) {
         Integer currentSize = actualPlayers.player.size();
         Player player = new Player();
@@ -155,14 +155,14 @@ class Kniffel {
         Integer goalValue = 0;
 
         // alle Würfel aufsummieren
-        if (goalName.equals(Language.CHANCE)) {
+        if (goalName.equals(Language.chance)) {
             for (int d = 0; d < 5; d++) {
                 goalValue = goalValue + actualPlayers.player.get(playerNumber).deck.dice[d].getCount();
             }
         }
         
         // Finde 5 gleiche Würfel, wenn ja 50 Punkte
-        if (goalName.equals(Language.KNIFFEL)) {
+        if (goalName.equals(Language.kniffel)) {
             Integer[] dices = dicesToArray(actualPlayers, playerNumber);            
             Boolean valid = Arrays.asList(dices).stream().allMatch(t -> t.equals(dices[1]));
             if (valid.equals(true)) goalValue = 50;      
@@ -170,7 +170,7 @@ class Kniffel {
         }
         
         // Finde 3 gleiche Würfel und addiere alle Augenzahlen    
-        if (goalName.equals(Language.THREE_EQUALS)) {                                                      
+        if (goalName.equals(Language.threeEquals)) {                                                      
             Boolean valid = countEqualDices(actualPlayers, playerNumber, 3);                        
             if (valid.equals(true)) {
                 for (int d = 0; d < 5; d++) {
@@ -180,7 +180,7 @@ class Kniffel {
         }
 
         // Finde 4 gleiche Würfel und addiere alle Augenzahlen  
-        if (goalName.equals(Language.FOUR_EQUALS)) {                      
+        if (goalName.equals(Language.fourEquals)) {                      
             Boolean valid = countEqualDices(actualPlayers, playerNumber, 4);            
             if (valid.equals(true)) {            
                 for (int d = 0; d < 5; d++) {
@@ -190,7 +190,7 @@ class Kniffel {
         }
 
         // Finde ein Paar und einen Drilling, wenn ja, 25 Punkte
-        if (goalName.equals(Language.FULL_HOUSE)) {            
+        if (goalName.equals(Language.fullHouse)) {            
             Boolean valid = false;
             Boolean valid1 = false; // Pair of three exists
             Boolean valid2 = false; // Pair of two exists
@@ -199,7 +199,7 @@ class Kniffel {
             Integer[] dices = dicesToArray(actualPlayers, playerNumber);
             valid = Arrays.asList(dices).stream().allMatch(t -> t.equals(dices[1]));
 
-            if (!valid) {
+            if (Boolean.FALSE.equals(valid)) {
                 Integer count = 0;
                 Integer pairOfThree = 0;
                 
@@ -215,7 +215,7 @@ class Kniffel {
                 if (count > 2) valid1 = true;
 
                 // gibt es noch ein weiteres Paar
-                if (valid1) {
+                if (Boolean.TRUE.equals(valid1)) {
                     for (int c = 1; c < 7; c++) {
                         if (c != pairOfThree) {
                             Integer tempCount = 0;
@@ -233,7 +233,7 @@ class Kniffel {
         }
 
         // Finde 4 aufeinander folgende Würfel, wenn ja 30 Punkte
-        if (goalName.equals(Language.SMALL_STREET)) {
+        if (goalName.equals(Language.smallStreet)) {
             Integer[] dices = dicesToArray(actualPlayers, playerNumber);
             Arrays.sort(dices);     
 
@@ -259,7 +259,7 @@ class Kniffel {
         }
         
         // Finde 5 aufeinander folgende Würfel, wenn ja 40 Punkte
-        if (goalName.equals(Language.BIG_STREET)) {
+        if (goalName.equals(Language.bigStreet)) {
             Integer[] dices = dicesToArray(actualPlayers, playerNumber);
             Arrays.sort(dices);            
             Boolean valid = true;
@@ -275,12 +275,12 @@ class Kniffel {
         }
 
         Integer goalCounter = 0;
-        if (goalName.equals(Language.ONES)) goalCounter = 1;
-        if (goalName.equals(Language.TWOS)) goalCounter = 2;
-        if (goalName.equals(Language.THREES)) goalCounter = 3;
-        if (goalName.equals(Language.FOURS)) goalCounter = 4;
-        if (goalName.equals(Language.FIVES)) goalCounter = 5;
-        if (goalName.equals(Language.SIXES)) goalCounter = 6;
+        if (goalName.equals(Language.ones)) goalCounter = 1;
+        if (goalName.equals(Language.twos)) goalCounter = 2;
+        if (goalName.equals(Language.threes)) goalCounter = 3;
+        if (goalName.equals(Language.fours)) goalCounter = 4;
+        if (goalName.equals(Language.fives)) goalCounter = 5;
+        if (goalName.equals(Language.sixes)) goalCounter = 6;
         
         if (goalCounter > 0) {         
             for (int d = 0; d < 5; d++) {
@@ -302,7 +302,7 @@ class Kniffel {
             if (actualPlayers.player.get(playerNumber).sheet.game.get(0).set.key.equals(goalName)) keyExists = true;
         }        
         
-        if (keyExists) return false; // Wurf existiert
+        if (Boolean.TRUE.equals(keyExists)) return false; // Wurf existiert
         else {
             Goal newGoal = new Goal();
             newGoal.set = new Tuple<>(goalName, goalValue);        
@@ -321,7 +321,7 @@ class Kniffel {
 
         if (log) {
             System.out.println(actualPlayers.player.get(playerNumber).name);
-            System.out.println(Language.HORIZONTAL_RULER);
+            System.out.println(Language.horizontalRuler);
         }
 
         for (int j=0; j < listPart1.length; j++) {
@@ -339,12 +339,12 @@ class Kniffel {
         }
 
         if (log) {
-            System.out.println(Language.HORIZONTAL_RULER);
-            System.out.println(Language.PART_SUM_PART1 + actualPlayers.player.get(playerNumber).partSumPart1);
-            System.out.println(Language.BONUS + actualPlayers.player.get(playerNumber).bonusPart1);
-            System.out.println(Language.DOUBLE_RULER);
-            System.out.println(Language.SUM_PART1 + actualPlayers.player.get(playerNumber).sumPart1);
-            System.out.println(Language.DASHED_RULER);
+            System.out.println(Language.horizontalRuler);
+            System.out.println(Language.partialSumPart1 + actualPlayers.player.get(playerNumber).partSumPart1);
+            System.out.println(Language.bonus + actualPlayers.player.get(playerNumber).bonusPart1);
+            System.out.println(Language.doubleRuler);
+            System.out.println(Language.sumPart1 + actualPlayers.player.get(playerNumber).sumPart1);
+            System.out.println(Language.dashedRuler);
         }
         table[listPart1.length + 1] = actualPlayers.player.get(playerNumber).partSumPart1;
         table[listPart1.length + 2] = actualPlayers.player.get(playerNumber).bonusPart1;
@@ -363,10 +363,10 @@ class Kniffel {
             if (log) System.out.println("");
         }
         if (log) {
-            System.out.println(Language.HORIZONTAL_RULER);
-            System.out.println(Language.SUM_PART2 + actualPlayers.player.get(playerNumber).sumPart2);
-            System.out.println(Language.DOUBLE_RULER);
-            System.out.println(Language.SUM + actualPlayers.player.get(playerNumber).sum);
+            System.out.println(Language.horizontalRuler);
+            System.out.println(Language.sumPart2 + actualPlayers.player.get(playerNumber).sumPart2);
+            System.out.println(Language.doubleRuler);
+            System.out.println(Language.totalSum + actualPlayers.player.get(playerNumber).sum);
             System.out.println("");
         }
         table[listPart1.length + 3 + listPart2.length + 1] = actualPlayers.player.get(playerNumber).sumPart2;
@@ -405,22 +405,38 @@ class Kniffel {
         actualPlayers.player.get(playerNumber).sum = part1 + actualPlayers.player.get(playerNumber).bonusPart1 + part2;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Players knifflers = new Players();
         int[] table = new int[25];
         
         addPlayer(knifflers, "Jarvis");
     
         System.out.println("Würfle zufällig:");
-        System.out.println(Language.HORIZONTAL_RULER);
+        System.out.println(Language.horizontalRuler);
         for (int d = 0; d < 5; d++) {
             knifflers.player.get(0).deck.dice[d].roll();
-            System.out.println(Language.DICE + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
+            System.out.println(Language.dice + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
+        }
+
+        knifflers.player.get(0).deck.dice[2].setRollState(false);
+        System.out.println("Würfle zufällig, aber Würfel 3 ist blockiert:");
+        System.out.println(Language.horizontalRuler);
+        for (int d = 0; d < 5; d++) {
+            knifflers.player.get(0).deck.dice[d].roll();
+            System.out.println(Language.dice + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
+        }
+
+        knifflers.player.get(0).deck.dice[4].setRollState(false);
+        System.out.println("Würfle erneut zufällig, aber Würfel 3 und 5 sind blockiert:");
+        System.out.println(Language.horizontalRuler);
+        for (int d = 0; d < 5; d++) {
+            knifflers.player.get(0).deck.dice[d].roll();
+            System.out.println(Language.dice + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
         }
 
         System.out.println("");
         System.out.println("Nun bauen wir uns die Würfel, wie wir sie brauchen:");
-        System.out.println(Language.HORIZONTAL_RULER);
+        System.out.println(Language.horizontalRuler);
         System.out.println("");
 
         knifflers.player.get(0).deck.dice[0].setCount(3);
@@ -430,27 +446,27 @@ class Kniffel {
         knifflers.player.get(0).deck.dice[4].setCount(3);
 
         for (int d = 0; d < 5; d++) {            
-            System.out.println(Language.DICE + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
+            System.out.println(Language.dice + d + " : " + knifflers.player.get(0).deck.dice[d].getCount());   
         }
 
         System.out.println("");
         System.out.println("Dieser Wurf könnte eines dieser Ergbnisse sein:");
-        System.out.println(Language.DOUBLE_RULER);
+        System.out.println(Language.doubleRuler);
         System.out.println("");
 
-        addGoal(knifflers, 0, Language.ONES);
-        addGoal(knifflers, 0, Language.TWOS);
-        addGoal(knifflers, 0, Language.THREES);
-        addGoal(knifflers, 0, Language.FOURS);
-        addGoal(knifflers, 0, Language.FIVES);
-        addGoal(knifflers, 0, Language.SIXES);
-        addGoal(knifflers, 0, Language.THREE_EQUALS);
-        addGoal(knifflers, 0, Language.FOUR_EQUALS);
-        addGoal(knifflers, 0, Language.FULL_HOUSE);
-        addGoal(knifflers, 0, Language.SMALL_STREET);
-        addGoal(knifflers, 0, Language.BIG_STREET);
-        addGoal(knifflers, 0, Language.KNIFFEL);
-        addGoal(knifflers, 0, Language.CHANCE);        
+        addGoal(knifflers, 0, Language.ones);
+        addGoal(knifflers, 0, Language.twos);
+        addGoal(knifflers, 0, Language.threes);
+        addGoal(knifflers, 0, Language.fours);
+        addGoal(knifflers, 0, Language.fives);
+        addGoal(knifflers, 0, Language.sixes);
+        addGoal(knifflers, 0, Language.threeEquals);
+        addGoal(knifflers, 0, Language.fourEquals);
+        addGoal(knifflers, 0, Language.fullHouse);
+        addGoal(knifflers, 0, Language.smallStreet);
+        addGoal(knifflers, 0, Language.bigStreet);
+        addGoal(knifflers, 0, Language.kniffel);
+        addGoal(knifflers, 0, Language.chance);        
 
         for (int x = 0; x < knifflers.player.size(); x++) {
             sumParts(knifflers, x);
@@ -458,7 +474,7 @@ class Kniffel {
         }       
 
         System.out.println("Ergebnisliste zur Weiterverarbeitung:");
-        System.out.println(Language.DASHED_RULER);
+        System.out.println(Language.dashedRuler);
         System.out.println(Arrays.toString(table));
     }
 }
